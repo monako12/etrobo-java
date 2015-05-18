75ec773f940e359d971c5f168e33e7f192d1e868
monako12 <monako1201@gmail.com>
2015-05-18 17:11:13 +0900

    lejosの入れ方

--- Forward.java
+++ Forward.java
@@ -2,21 +2,21 @@
 *Forward.java
 */
 
-import josx.platform.rcx.Motor;
+import lejos.nxt.Motor;
 
 public class Forward{
 	public static void main(String[] args) {
-	// 駆動輪のモータのパワーを設定
-	Motor.C.setPower(3);
-	// 駆動輪のモータを晴天させる
-	Motor.C.forward();
+	Motor.C.setSpeed(720);
+	Motor.B.setSpeed(720);
+
+	Motor.C.backward();
+	Motor.B.backward();
+	
 	try{
-		//メインスレッドを2秒間スリープする
-		//この間モータは回転し続けている
-		Thred.sleep(2000);
+		Thread.sleep(6000);
 	} catch(Exception e){
 	}
-	// 駆動輪のモータを制止
 	Motor.C.stop();
+	Motor.B.stop();
 	}
 	}
