7236ab9e914d7e9096006b6a4dfb11e9a84d5eb1
e135754 <e135754@ie.u-ryukyu.ac.jp>
2015-05-25 17:44:39 +0900

    add ColorChecker.java

--- black.java
+++ black.java
@@ -2,6 +2,7 @@ public class black {
     public static void main(String[] args){
         LineCheck linecheck = new LineCheck();
         DriveMode drivemode = new DriveMode();
+        ColorChecker colorchecker = new ColorChecker();
 
         drivemode.Outline();
         linecheck.Check();
