7b495cc8792838733d259c48d675249c8098fe14
monako12 <monako1201@gmail.com>
2015-05-25 17:14:49 +0900

    mv

--- okada/LTCon.java
+++ okada/LTCon.java
@@ -7,28 +7,31 @@ public class LTCon{
 	TouchSensor ts = new TouchSensor(SensorPort.S2);
 	int a = 0;
 	boolean b = false;
+    boolean c = false;
+    boolean d = false;
 	while(a < 3){
 	    b = ts.isPressed();
+
+
 	    if(b == true){
 		if(a == 0){
 		    try{
-		    b = false;
 		    }catch(Exception e){}
 		    LCD.drawInt(ls.readValue() ,0,1);
 		    //		    ls.setLow(ls.readValue());
 		    a++;
-		    b = false;
-		}if(a == 1){ 
+		    TouchSensor ts2 = new TouchSensor(SensorPort.S2);
+		}if(a == 1 && c == true){
 		    try{
 			Thread.sleep(4000);
-			b = false;
 		    }catch(Exception e){}
 		    LCD.drawInt(ls.readValue() ,0,2);
 		    //  ls.setHigh(ls.readValue() );
 		    a++;
-		    b = false;
+                TouchSensor ts3 = new TouchSensor(SensorPort.S3);
 
-		}if(a == 2){
+
+		}if(a == 2 && c == true){
 		    try{
 		    Thread.sleep(2000);
 		    }catch(Exception e){}
@@ -36,7 +39,8 @@ public class LTCon{
 		    b = false;
 
 		}
-	    
+            c = ts2.isPressed();
+            d = ts3.isPressed();
 	}
 
 	}
