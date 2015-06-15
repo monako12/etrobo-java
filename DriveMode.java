ad0edb2bcc28f8054e61ba548c4f9d3dde2b6a9e
monako12 <monako1201@gmail.com>
2015-06-15 17:56:47 +0900

    change drivemode
    後輪の調整

--- DriveMode.java
+++ DriveMode.java
@@ -2,19 +2,23 @@ import java.*;
 
 import lejos.nxt.*;
 public class DriveMode { //how to run
-	String states;
-	int a;
-	public void Forward(int fw){
-		a = Math.abs(Motor.A.getTachoCount());
-		LCD.drawInt(a, 0, 4);
-		if(a < 500 || (0 > fw * Motor.A.getTachoCount())) {
-			Motor.A.setSpeed(500);
-			Motor.A.rotate(fw);
-		}
-
-		Motor.B.setSpeed(50);
-		Motor.C.setSpeed(50);
-		Motor.B.backward();
-		Motor.C.backward();
+    String states;
+    int a;
+    float b;
+    public void Forward(int fw){
+	a = Math.abs(Motor.A.getTachoCount());
+	b = Math.signum(fw);
+	LCD.drawInt(a, 0, 4);
+	if(a < 500 || (0 > fw * Motor.A.getTachoCount())) {
+	    Motor.A.setSpeed(500);
+	    Motor.A.rotate(fw);
 	}
+	b = 20*b;
+	Motor.B.setSpeed(50+b);
+	Motor.C.setSpeed(50-b);
+	Motor.B.backward();
+	Motor.C.backward();
+	
+    }
+    
 }
